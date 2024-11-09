4fef27383af16da43cb381c8a011caa7eb249269
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-11-09 08:49:25 -0500

    Get node using index with at method

--- lib/LinkedList.rb
+++ lib/LinkedList.rb
@@ -2,16 +2,15 @@ require_relative 'Node'
 
 class LinkedList
   attr_accessor :head, :size
-  
+
   def initialize()
     self.head = nil
-    self.size = 0
   end
 
   def crawl(size = false)
     curr = head
     nodes = 1
-    while yield(curr)
+    while yield(curr, nodes)
       curr, nodes = curr.next, nodes + 1
     end
     size ? nodes : curr
@@ -21,17 +20,15 @@ class LinkedList
     if head.nil?
       @head = Node.new(value)
     else
-      tail = crawl { |curr| !curr.next.nil? }
+      tail = crawl {|curr| !curr.next.nil?}
       tail.next = Node.new(value)
     end
-    @size += 1
   end
 
   def prepend(value)
     new_node = Node.new(value)
     new_node.next = head
     @head = new_node
-    @size += 1
   end
 
   def size
@@ -47,6 +44,11 @@ class LinkedList
     puts curr.value
   end
 
+  def at(index)
+    raise "***index must be positive***" if index < 0
+    curr = crawl {|curr, nodes| index >= nodes }
+    puts curr ? curr.value : "index #{index} out of range"
+  end
 
   def display
     crawl do |curr|
--- main.rb
+++ main.rb
@@ -1,12 +1,24 @@
 require_relative 'lib/LinkedList'
 
+#***On problem 6, thinking about putting processes here to pass into these methods
+
 list = LinkedList.new()
 list.append(2)
 list.append(3)
 list.append(4)
+list.prepend(1)
 list.display
 list.size
 list.get_tail
+list.prepend("apple")
+list.display
+list.at(0)
+list.at(1)
+list.at(2)
+list.at(3)
+list.at(4)
+list.at(5)
+list.at(-1)
 # list.prepend(1)
 # list.display
 # list.size
